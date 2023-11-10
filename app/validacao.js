7573348363ec68329536c96dbc0788209ddcf478
Marcus Paraense <marcus_paraense@yahoo.com.br>
2023-11-10 16:29:07 -0300

    atualização game over

--- app/validacao.js
+++ app/validacao.js
@@ -1,9 +1,20 @@
 function verificarSeOChutePossuiUmValorValido (chute) {
     const numero = +chute;
-    
+    const chuteString = chute.toLowerCase();
+    console.log(numero);
+    console.log(chute);
+  
     if(chuteForInvalido(numero)) {
+        if(chuteString.includes('game over')) {  
+        document.body.innerHTML = `
+        <h2><span>GAME OVER</span><h2>
+        <h3>Obrigado por Jogar<h3>
+        <button id="jogar-novamente" class="btn-jogar">Jogar novamente</button>
+        `;
+        } else {
         console.log('valor invalido. Fale um numero válido.');
         elementoChute.innerHTML += `<div>valor invalido. Fale um numero válido.</div>`
+        }
     } else if(numeroForMaiorOuMenorQueOValorPermitido(numero)) {
         console.log(`valor invalido: numero secreto precisa estar entre ${menorValor} e ${maiorValor}`);
         elementoChute.innerHTML += `<div>valor invalido: Fale numero entre ${menorValor} e ${maiorValor}</div>`;
@@ -12,8 +23,7 @@ function verificarSeOChutePossuiUmValorValido (chute) {
         <h2>Você Acertou!!</h2>
         <h3>O número secreto era ${numeroSecreto}</h3>
         <button id="jogar-novamente" class="btn-jogar">Jogar novamente</button>
-        `
-        
+        `        
     } else if (numero < numeroSecreto) {
         elementoChute.innerHTML +=
         `<div>O número secreto é maior <i class="fa-solid fa-arrow-up-long"></i></div>`;
@@ -37,3 +47,4 @@ document.body.addEventListener('click', e => {
         window.location.reload();
     }
 })
+
